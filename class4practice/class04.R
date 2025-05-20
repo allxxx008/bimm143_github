566dd68d6559fac624750af5a83fb5f2b288ea1c
Allen Xu <alxu@ucsd.edu>
2025-05-20 11:55:28 -0700

    class 4 changes

---
+++ class4practice/class04.R
@@ -0,0 +1,11 @@
+#' ---
+#' title: "Intro to R"
+#' author: "Allen Xu A16897142"
+#' date: "April 10th, 2025"
+#' ---
+# My first R script
+x <- 1:50
+plot(x,sin(x))
+plot (x,sin(x),typ="l",col="red",lwd=8,
+      xlab="Silly x axis", ylab="Sensible y axis")
+
